11ddce16ec9e5f2f6c71171b8682ef0b5be9351d
trungnt9391 <ntt9391@gmail.com>
2023-10-16 03:53:11 +0700

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,7 +69,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            logging.warning('Unsuccessful login attempt for user: {}'.format(form.username.data))
+            logging.warning('Unsuccessful login attempt for user:- {}'.format(form.username.data))
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
